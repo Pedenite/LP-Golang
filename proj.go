cc1fd1da19c58ff0544db0b822971ecc2c638ca5
Pedenite <pedenite@gmail.com>
2019-11-07 19:08:59 -0300

    ler arquivo

---
+++ proj.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	var frases []string
+
+	file, err := os.Open("fileName.txt")
+	if err != nil { //tratamento de erro
+		fmt.Println(err)
+	}
+	reader := bufio.NewReader(file)
+	for {
+		linha, err := reader.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		frases = append(frases, linha)
+
+		if err == io.EOF {
+			break
+		}
+
+	}
+	for i := 0; i < 3; i++ {
+		fmt.Println(frases[i])
+	}
+	file.Close()
+}
