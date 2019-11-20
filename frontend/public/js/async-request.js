f360e02399b93bf5c12d15153bdac511fedfe64c
KeydsonEstrela <keydsonestrela@hotmail.com>
2019-11-20 11:01:55 -0200

    Mudanças na Circle Barrier Progress

--- frontend/public/js/async-request.js
+++ frontend/public/js/async-request.js
@@ -70,9 +70,9 @@ function getTamanhoLista() {
                 tamanhoLista = aprendidas + data
             }
 
-            qntLoader('totalQnt', tamanhoLista, 'Total', "#ffffff", "#083975", "#083975");
-            qntLoader('aprendidasQnt', ((aprendidas*100)/tamanhoLista), 'Aprendidas',"#ffffff", "#00f7ff", "#ffffff");
-            qntLoader('emAndamentoQnt', 100 - ((aprendidas*100)/tamanhoLista), 'Andamento',"#a6ffce","#00ff72","#a6ffce");
+            qntLoader('totalQnt', tamanhoLista, 'Total', "#ffffff", "#083975", "#083975", 0);
+            qntLoader('aprendidasQnt', ((aprendidas*100)/tamanhoLista), 'Aprendidas',"#ffffff", "#00f7ff", "#ffffff", null);
+            qntLoader('emAndamentoQnt', 100 - ((aprendidas*100)/tamanhoLista), 'Andamento',"#a6ffce","#00ff72","#a6ffce", null);
 
             $('.total-value').text(tamanhoLista);
             $('.aprendidas-value').text(aprendidas);
@@ -99,17 +99,17 @@ function addFrase(original, traducao) {
         }
     });
 }
-function qntLoader(idContainer, progress, title, colorNum, colorProgress, colorBarrier) {
+function qntLoader(idContainer, progress, title, colorNum, colorProgress, colorBarrier, progressVal) {
     $(`#${idContainer}`).circularloader({
         backgroundColor: "#083975",//background colour of inner circle
         fontColor: colorNum,//font color of progress text
         fontSize: "25px",//font size of progress text
-        radius: 35,//radius of circle
+        radius: 40,//radius of circle
         progressBarBackground: colorBarrier ,//background colour of circular progress Bar
         progressBarColor: colorProgress,//colour of circular progress bar
         progressBarWidth: 15,//progress bar width
         progressPercent: progress,//progress percentage out of 100
-        progressValue: progress,//diplay this value instead of percentage
+        progressValue: progressVal,//diplay this value instead of percentage
         showText: true,//show progress text or not
         title: title,//show header title for the progress bar
     });
